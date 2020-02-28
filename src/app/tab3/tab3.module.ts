67900210898d5b24a905615429e98bc6d5f747a0
Olsi Karapici <olsikarapici@gmail.com>
2020-02-28 10:05:37 +0100

    removed container component used in tabs, removed styling of tabs being fullpage, made headers visibile in all tabs

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,16 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }])
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule { }
