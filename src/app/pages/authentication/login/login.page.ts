9448212e3051d0bb04ececb32bf2928fef6d19f0
Olsi Karapici <olsikarapici@gmail.com>
2020-02-28 15:53:03 +0100

    implemented login page

--- src/app/pages/authentication/login/login.page.ts
+++ src/app/pages/authentication/login/login.page.ts
@@ -1,4 +1,8 @@
+import { LoadingService } from './../../../services/loading.service';
+import { AuthenticationService } from './../../../services/authentication.service';
+import { User } from './../../../interfaces/user';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public error: string;
+  user: User;
+
+  constructor(
+    private router: Router,
+    private loadingService: LoadingService,
+    private auth: AuthenticationService
+  ) {
+    this.error = '';
+    this.user = {};
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+
+
+  forgotPassword() {
+    this.router.navigate(['/forgot-password']);
+  }
+
+  ionViewWillEnter() {
+    this.error = '';
+  }
+
+  login(loginFormData) {
+    this.error = '';
+    this.loadingService.present();
+
+    this.auth.signIn(loginFormData.email, loginFormData.password).then((data) => {
+      this.router.navigate(["home"]).then(() => {
+        if (this.loadingService.isLoading)
+          this.loadingService.dismiss();
+      })
+      console.log('sign in ok return then promise');
+      console.log(data);
+
+    }, (err) => {
+
+      if (this.loadingService.isLoading) {
+        this.loadingService.dismiss()
+      }
+
+      console.log('error: ', err);
+      this.error = '';
+      this.error = err.message;
+
+    });
+  }
 
   ngOnInit() {
   }
-
 }
